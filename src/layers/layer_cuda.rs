4a3cefe45987d470c147075aec114a7e56515d7a
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-05-30 11:33:24 +0800

    Update layer_cuda.rs
    Added constructors for all composite structs

--- src/layers/layer_cuda.rs
+++ src/layers/layer_cuda.rs
@@ -1,4 +1,4 @@
-use std::os::raw::c_void;
+use std::{os::raw::c_void, process::Stdio};
 
 use crate::neuralnet::TraversePtrs;
 
@@ -21,26 +21,60 @@ pub struct IOPtrs
     pub in_shape: (usize, usize, usize),
     pub out_shape: (usize, usize, usize),
 
-    pub output_ptr: *mut f32,
-    pub output_grad_ptr: *mut f32,
-
     pub input_ptr: *mut f32,
     pub input_grad_ptr: *mut f32,
 
-    pub output_traverse_ptr: *mut TraversePtrs,
+    pub output_ptr: *mut f32,
+    pub output_grad_ptr: *mut f32,
+
+    pub output_traverse_ptr: Option<Box<TraversePtrs>>,
 }
+impl IOPtrs
+{
+    pub fn new(in_shape: (usize, usize, usize), out_shape: (usize, usize, usize)) -> Self
+    {
+        return Self
+        {
+            in_shape, out_shape,
+            input_ptr: std::ptr::null_mut(),
+            input_grad_ptr: std::ptr::null_mut(),
+            output_ptr: std::ptr::null_mut(),
+            output_grad_ptr: std::ptr::null_mut(),
+            output_traverse_ptr: None,
+        }
+    }
+}
+
 
 pub struct ParameterPtrs
 {
-    pub biases_ptr: *mut f32,
-    pub bias_grad_ptr: *mut f32,
-    pub bias_vel_ptr: *mut f32,
-    pub bias_moment_ptr: *mut f32,
-
     pub weight_ptr: *mut f32,
     pub weight_grad_ptr: *mut f32,
     pub weight_vel_ptr: *mut f32,
     pub weight_moment_ptr: *mut f32,
+
+    pub biases_ptr: *mut f32,
+    pub bias_grad_ptr: *mut f32,
+    pub bias_vel_ptr: *mut f32,
+    pub bias_moment_ptr: *mut f32,
+}
+impl ParameterPtrs
+{
+    pub fn new() -> Self
+    {
+        return Self
+        {
+            weight_ptr: std::ptr::null_mut(),
+            weight_grad_ptr: std::ptr::null_mut(),
+            weight_vel_ptr: std::ptr::null_mut(),
+            weight_moment_ptr: std::ptr::null_mut(),
+        
+            biases_ptr: std::ptr::null_mut(),
+            bias_grad_ptr: std::ptr::null_mut(),
+            bias_vel_ptr: std::ptr::null_mut(),
+            bias_moment_ptr: std::ptr::null_mut(),
+        }
+    }
 }
 
 pub struct AllocationStatus
@@ -49,26 +83,67 @@ pub struct AllocationStatus
     pub bias_ptr_allocated: bool, 
     pub weight_array_allocated: bool,
     pub bias_array_allocated: bool,
-    
-    pub use_bias: bool,
-    
+        
     pub zero_output: bool,
     pub zero_input_grad: bool,
     pub zero_weight_grad: bool
 }
+impl AllocationStatus
+{
+    pub fn new() -> Self
+    {
+        return Self
+        {
+            weight_ptr_allocated: false,
+            bias_ptr_allocated: false,
+            weight_array_allocated: false,
+            bias_array_allocated: false,
+
+            zero_output: true,
+            zero_input_grad: false,
+            zero_weight_grad: false
+        }
+    }
+}
 
 pub struct MiscData
 {
-    pub backward_count: Box<f32>,
-    pub backward_count_weight_prev: Box<f32>,
-    pub backward_count_in_prev: Box<f32>,
+    pub backward_count: Option<Box<f32>>,
+    pub backward_count_weight_prev: Option<Box<f32>>,
+    pub backward_count_in_prev: Option<Box<f32>>,
 
     pub batch_size: f32,
     pub count: u128,
+}
+impl MiscData
+{
+    pub fn new(batch_size: f32) -> Self
+    {
+        return Self
+        {
+            backward_count: None,
+            backward_count_weight_prev: None,
+            backward_count_in_prev: None,
+
+            batch_size,
+            count: 0
+        }
+    }
 }
 
 pub struct WeightTensors
 {
     pub weight: Vec<f32>,
     pub biases: Vec<f32>,
+}
+impl WeightTensors
+{
+    pub fn new() -> Self
+    {
+        return Self
+        {
+            weight: Vec::new(),
+            biases: Vec::new()
+        }
+    }
 }
