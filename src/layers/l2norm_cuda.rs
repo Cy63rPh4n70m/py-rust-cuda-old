1133a2b3d853b37c76ec1f0a61d9ba26b4b4d8dd
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-06-02 11:42:54 +0800

    Update l2norm_cuda.rs
    - matched forward method definition with trait defintion
    - variable name changes
    - remove unnecessary comments

--- src/layers/l2norm_cuda.rs
+++ src/layers/l2norm_cuda.rs
@@ -29,62 +29,39 @@ impl L2NormCuda
 impl LayerCuda for L2NormCuda
 {
     // supports batch matrix multiplication unlike cpu
-    pub fn forward(&mut self, str_ptr_in: String) -> String
+    fn forward(&mut self, trav_ptr_in: *mut TraversePtrs, _trav_ptr_weight: *mut TraversePtrs, _use_dropout: bool) -> *mut TraversePtrs
     {
-        let batch: usize = self.shape.0;
-        let rows: usize = self.shape.1;
-        let cols: usize = self.shape.2;
+        let batch: usize = self.io_ptrs.in_shape.0;
+        let rows: usize = self.io_ptrs.in_shape.1;
+        let cols: usize = self.io_ptrs.in_shape.2;
 
-        if !self.ptrs_allocated
+        if !self.allocation_status.ptrs_allocated
         {   
             // initialise input pointer, set the input as the result pointer from previous layer
             // tensor struct at this stage will contain the result ptr of the previous layer
             ////////////////////////////////////////////////////////////////
-            self.input_pow2_ptr = new_cuda_ptr_str(&[batch, rows, cols]);
+            self.input_pow2_ptr = new_cuda_array((batch * rows * cols) as u32);
             
             // initialise the result tensor/pointer
-            self.power_sum = new_cuda_ptr_str(&[batch, rows, 1]);
+            self.power_sum = new_cuda_array((batch * rows * 1) as u32);
 
-            init_layer_connections(
-                &mut self.backward_count, &str_ptr_in, 
-                &mut self.backward_count_prev, &mut self.input_ptr, 
-                &mut self.input_grads_ptr, &mut self.output_ptr, 
-                &mut self.output_grads_ptr, &mut self.output_traverse_ptr, 
-                &[self.shape.0, self.shape.1, self.shape.2]
+            init_trav_in_ptrs(
+                &trav_ptr_in, &mut self.io_ptrs.backward_count,
+                &mut self.io_ptrs.backward_count_in_prev, 
+                &mut self.io_ptrs.input_ptr, &mut self.io_ptrs.input_grad_ptr, 
+                &mut self.io_ptrs.output_ptr, &mut self.io_ptrs.output_grad_ptr, 
+                &mut self.io_ptrs.output_traverse_ptr, 
+                (self.io_ptrs.out_shape.0 * self.io_ptrs.out_shape.1 * self.io_ptrs.out_shape.2) as usize
             );
             
-            self.ptrs_allocated = true;
+            self.allocation_status.ptrs_allocated = true;
+            self.allocation_status.arrays_allocated = true;
         }
 
-        //let broadcast_buf: String = new_cuda_ptr_str(batch * rows * cols * self.n_out);
-
-        // convert strings to pointers
-        let input_ptr: *mut f32 = string_to_ptr(&self.input_ptr);
-        let input_pow2_ptr: *mut f32 = string_to_ptr(&self.input_pow2_ptr);
-        let power_sum: *mut f32 = string_to_ptr(&self.power_sum);
-        let result_ptr: *mut f32 = string_to_ptr(&self.output_ptr);
-
-        // data does not need to be copied as result ptr from previous layer
-        // is set as the input
-
-        // copy data to the input pointer, prevent reallocation
-        //copy_cuda_to_cuda(
-        //    input_ptr, 
-        //    input.get_ptr(), 
-        //    &[batch, rows, cols]
-        //);
-
-        // parallel perform matrix multiplication
-        // and sum with bias tensor
-        // result pointer updated
-        //let start: Instant = Instant::now();
-        //if self.use_tiled || !self.use_tiled
-        //{
-        //println!("{:?}", cuda_ptr_to_array(input_ptr, &[batch, rows, cols]));
         l2norm_forward(
-            input_ptr, input_pow2_ptr, 
+            self.io_ptrs.input_ptr, self.input_pow2_ptr, 
             batch, rows, cols, 
-            power_sum, result_ptr, true
+            self.power_sum, self.io_ptrs.output_ptr, true
         );
 
         //println!("{:?}", cuda_ptr_to_array(input_ptr, &[batch, rows, cols]));
@@ -92,34 +69,9 @@ impl LayerCuda for L2NormCuda
         //println!("{:?}", cuda_ptr_to_array(result_ptr, &[batch, rows, cols]));
         //exit(1);
 
-        set_zero_counter(&self.backward_count);
+        set_zero_counter(self.io_ptrs.backward_count);
 
-        return self.output_traverse_ptr.clone();
-        //println!("{:?}", cuda_ptr_to_array(power_sum, &[batch, rows, 1]));
-        //println!("{:?}", cuda_ptr_to_array(result_ptr, &[batch, rows, cols]));
-        //}
-        //else
-        //{
-        //    matmul_add_bias(
-        //        input_ptr, batch as u32, rows as u32, cols as u32, 
-        //        weight_ptr, batch as u32, cols as u32, self.n_out as u32,
-        //        result_ptr, bias_ptr
-        //    );
-        //}
-        //let end = start.elapsed();
-        //println!("dense: {:.6}", end.as_secs_f64());
-
-        //println!("-----------------------------");
-        //println!("{:?}", cuda_ptr_to_array(input_ptr, &[batch, rows, cols]));
-        //println!("{:?}", cuda_ptr_to_array(weight_ptr, &[batch, cols, self.n_out]));
-        // overwrite the current pointer with result ptr, to be COPIED to input of next layer
-        // current pointer is already recorded by previous layer, don't free
-
-        //println!("{:?}", cuda_ptr_to_array(input.get_ptr(), input.get_shape()));
-        //println!("-----------------------------");
-        //exit(1);
-        // previous pointer will be recorded in previous layer
-
+        return self.io_ptrs.output_traverse_ptr;
     }
 
     pub fn backward(&mut self)
