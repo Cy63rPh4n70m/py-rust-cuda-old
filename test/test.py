4995ffbb24256fca3d4d7fac87f01838963adf54
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-06-03 11:42:04 +0800

    Temporary removal
    Removed test directory

--- test/test.py
+++
@@ -1,8 +0,0 @@
-from Model import Model
-import numpy as np
-model = Model()
-
-inputs = np.random.uniform(-1.0, 1.0, size=(1, 1, 20))
-output = model.forward(inputs)
-print(output)
-model.details()
