ab292bf41716f808174ac1b421fef1c4886f3433
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-06-05 12:15:19 +0800

    Update layer_cuda.rs
    Added default implementations for methods

--- src/layers/layer_cuda.rs
+++ src/layers/layer_cuda.rs
@@ -11,10 +11,16 @@ pub trait LayerCuda
     fn backward(&mut self, use_dropout: bool);
     fn update_params(&mut self, optimizer_type: i32, lr: f32, l2: f32, alpha: f32, beta: f32);
     fn details(&self);
-    fn get_param_count(&self) -> usize;
-    fn move_ptrs_to_arrays(&mut self);
-    fn get_weights_hashmap(&mut self) -> Option<HashMap<&str, Vec<f32>>>;
-    fn load_weights_from_hashmap(&mut self, hashmap: &HashMap<&str, Vec<f32>>);
+
+    fn get_param_count(&self) -> usize
+    {
+        return 0_usize;
+    }
+    fn move_ptrs_to_arrays(&mut self) {}
+    fn get_weights_hashmap(&mut self) -> Option<HashMap<&str, Vec<f32>>> {
+        return None;
+    }
+    fn load_weights_from_hashmap(&mut self, _hashmap: &HashMap<&str, Vec<f32>>) {}
 }
 
 // composition structs to reduce code repetition
