131844e28c608398032f64e91ba655fba6be49c5
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-06-07 13:13:00 +0800

    Added deprecation comments

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,5 @@
+//! Custom type definitions here are specifically for loss
+//! and loss_grads FFI functions, subject to removal in the future
 use ndarray::ArrayD;
 
 pub type LossFn = fn(ArrayD<f32>, ArrayD<f32>) -> f32;
