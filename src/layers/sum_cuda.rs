56ae98de6845a685ab4c09a44e31f7bff9b458ad
Cy63rPh4n70m <103024336+Cy63rPh4n70m@users.noreply.github.com>
2025-06-08 01:21:12 +0800

    Updated comments

--- src/layers/sum_cuda.rs
+++ src/layers/sum_cuda.rs
@@ -5,6 +5,7 @@ use crate::{
 
 use super::layer_cuda::{AllocationStatus, IOPtrs, LayerCuda};
 
+/// performs summing along axis of an array
 pub struct SumCuda
 {
     pub io_ptrs: IOPtrs,
@@ -44,9 +45,10 @@ impl SumCuda
 
 }
 impl LayerCuda for SumCuda
-{    // supports batch matrix multiplication unlike cpu
+{
     fn forward(&mut self, trav_ptr_in: *mut TraversePtrs, _trav_ptr_weight: *mut TraversePtrs, _use_dropout: bool) -> *mut TraversePtrs
     {
+        // link with previous layer
         if !self.allocation_status.ptrs_allocated
         {
             init_trav_in_ptrs(
@@ -62,15 +64,15 @@ impl LayerCuda for SumCuda
             self.allocation_status.arrays_allocated = true;
         }
 
+        // CUDA axis sum
         sum_axis(
             self.io_ptrs.output_ptr, self.io_ptrs.input_ptr, 
             self.io_ptrs.in_shape.0, self.io_ptrs.in_shape.1, self.io_ptrs.in_shape.2, 
             self.axis, self.allocation_status.zero_output
         );
 
+        // important for zeroing gradients during backward pass
         set_zero_counter(self.io_ptrs.backward_count);
-        //println!("input: {:?}\n", cuda_ptr_to_array(input_ptr, &[self.shape.0, self.shape.1, self.shape.2]));
-        //println!("results: {:?}\n", cuda_ptr_to_array(result_ptr, new_slice_dim.as_slice()));
 
         return self.io_ptrs.output_traverse_ptr;
 
@@ -78,11 +80,14 @@ impl LayerCuda for SumCuda
 
     fn backward(&mut self, _use_dropout: bool)
     {
+        // only zero input gradients when previous layer's
+        // backward counter is zero
         if !counter_is_zero(self.io_ptrs.backward_count_in_prev)
         {
             self.allocation_status.zero_input_grad = false;
         }
 
+        // opposite of summing is broadcasting
         broadcast_2d_to_3d(
             self.io_ptrs.input_grad_ptr, 
             self.io_ptrs.in_shape.0, self.io_ptrs.in_shape.1, self.io_ptrs.in_shape.2, 
@@ -90,19 +95,7 @@ impl LayerCuda for SumCuda
         );
 
         self.batch_size += 1.0;
-        increment_counter(self.io_ptrs.backward_count_in_prev);
-        
-        //let end = start.elapsed();
-        //println!("backward: {:.6}", end.as_secs_f64());
-
-        
-        //println!("original_grads: {:?}\n", cuda_ptr_to_array(&[self.shape.0, self.shape.1, self.shape.2]));
-        //ptr.set_ptr(input_grad_ptr, vec![self.shape.0, self.shape.1, self.shape.2]);
-        //println!("input_gradients: {:?}\n", cuda_ptr_to_array(ptr.get_ptr(), ptr.get_shape()));
-        //println!("weight_gradients: {:?}\n", cuda_ptr_to_array(input_grad_ptr, &[self.shape.0, self.shape.1, self.shape.2]));
-        //println!("mask: {:?}\n", cuda_ptr_to_array(mask_ptr, &[self.shape.0, self.shape.1, self.shape.2]));
-        //println!("=========================================================");
-        //exit(1);      
+        increment_counter(self.io_ptrs.backward_count_in_prev);   
     }
 
     fn update_params(&mut self, _optimizer_type: i32, _lr: f32, _l2: f32, _alpha: f32, _beta: f32)
